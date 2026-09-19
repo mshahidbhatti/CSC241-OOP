a6509240641738b10949ec0ff5fd714016ac6384
Muhammad Shahid Bhatti <mshahidbhatti@gmail.com>
2026-09-19 17:52:09 +0500

    CSC241 update - 2026-09-19 17:52 - BCS Lecture 4

---
+++ 01_BCS/01_Weekly_Delivery/Week02/Lecture02/Add.java
@@ -0,0 +1,17 @@
+public class Add{
+
+	public int add(int a, int b){   // add(int, int)
+		System.out.println("Sum of two numbers :"+(a+b));
+		return a+b;
+	}
+
+
+	public double add(double a, double b){   // add(double, int)
+		System.out.println("Sum of two  decimal numbers :"+(a+b));
+		return a+b;
+	}
+
+
+
+
+}
---
+++ 01_BCS/01_Weekly_Delivery/Week02/Lecture02/Demo.java
@@ -0,0 +1,13 @@
+public class Demo{
+
+	public static void main(String args[]){
+		Add obj1=new Add();
+		obj1.add(4,5);
+
+		obj1.add(5.0,5);
+
+	}
+
+}
+
+// byte -> short -> int -> long -> float -> double
